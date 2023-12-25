4ab3aaefec0d46da13d276693774b209970a832c
emaema1128 <y.on.dw3@gmail.com>
2023-12-25 11:38:33 +0900

    デプロイ準備

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+# postgres://protospace_40118_user:uAIqMBrRQfxYrSeRFjz6hEH4ADErn0JQ@dpg-cm4ecui1hbls73ad642g-a/protospace_40118
